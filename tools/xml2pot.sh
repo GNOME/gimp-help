4a6e49a3997a6ce16b91baee710129d8a0dde438
Jacob Boerema <jgboerema@gmail.com>
2025-07-29 11:59:24 -0400

    Fix pot output on Debian
    Can't use `sh` in shebang, since not all shells support the
    substitution we are using. So explicitly set `bash` until we
    find a better solution.

--- tools/xml2pot.sh
+++ tools/xml2pot.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+# Using bash here explicitly instead of sh to allow substitution of "${@:4}"
 #
 # Create a PO-template (POT)
 # Since meson can't do piped commands we use a script
@@ -7,10 +8,11 @@
 #
 # Assumptions: this script is in the same directory as xml2po
 #
-# First argument $1 - name of output.pot
-# $2 - build root
-# $3 = source root
-# Arguments $4-...  - xml input files
+# Arguments (starting at 1)
+# $1 - name of output.pot (including subdir)
+# $2 - build root (added to output.pot)
+# $3 - source root (to find xml2po.py)
+# $4 and up ... - xml input files (already including full path)
 #
 # Ideally we would want to be able to set where
 # xml2po, msguniq, msgcat etc. are located but with current (0.50) meson setting
@@ -18,22 +20,22 @@
 
 build_root=$2
 source_root=$3
-#out_pot="$MESON_BUILD_ROOT/pot/$1"
-#out_pot="$MESON_BUILD_ROOT/pot/$1"
-#out_pot=$build_root/pot/$1
 out_pot=$build_root/$1
+
+# Put input arguments 4 and up into src_files
 src_files="${@:4}"
 
 # Keeping these debug statments for now...
-echo Meson source: $MESON_SOURCE_ROOT
-echo Meson build: $MESON_BUILD_ROOT
+# These 2 env vars seem to be always empty
+#echo Meson source: $MESON_SOURCE_ROOT
+#echo Meson build: $MESON_BUILD_ROOT
 
-echo First argument: $1
-echo Meson source: $source_root
-echo Meson build: $build_root
-echo Source files: ${@:4}
-echo Source files: $src_files
-echo Destination file: $out_pot
+#echo First argument: $1
+#echo Meson source: $source_root
+#echo Meson build: $build_root
+#echo Source files: "${@:4}"
+#echo Source files: $src_files
+#echo Destination file: $out_pot
 
 $source_root/tools/xml2po.py --mode=gimphelp --output=- $src_files \
   | msguniq | msgcat - --width=79 -o "$out_pot"
