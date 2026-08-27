cb5954eb247c7d4e9406ed937b0c9af3b77d22c0
Jacob Boerema <jgboerema@gmail.com>
2026-08-27 15:14:24 -0400

    build: Ignore LINGUAS in validate_po.py
    Alternatively we could only allow .po and .pot

--- tools/validate_po.py
+++ tools/validate_po.py
@@ -349,7 +349,7 @@ def main(argv):
 
     validator = Validate(verbose, warnings)
     for file in filenames:
-        if not file.endswith('meson.build'):
+        if not (file.endswith('meson.build') or file.endswith('LINGUAS')):
             if verbose:
                 print(f"Checking {file}")
             validator.setFile(file)
