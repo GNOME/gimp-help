630a013dc35056bccd8b191420ef97aeb3ef2192
Jacob Boerema <jgboerema@gmail.com>
2026-09-08 08:50:19 -0400

    make.d/make_image_links.pl: filter out meson.build files...
    that now exist in image folders

--- make.d/make_image_links.pl
+++ make.d/make_image_links.pl
@@ -172,29 +172,33 @@ foreach my $srcdir (sort @Image_dirs) {
         next unless -f $imgfile;
         my $basename = (splitpath($imgfile))[2];  # (vol, dir, file)
         my $destfile = catfile($dstdir, $basename);  # the new file/link
-        # Check for existence of localized image:
-        if ($Localize_images) {
-            (my $localized_imgfile = $imgfile) =~ s|/C/|/$Language/|o;
-            $inc_i18n = (-e $localized_imgfile) ? 1 : 0;
-            $imgfile = $localized_imgfile if -e _;  # if $inc_i18n;
+        #skip meson.build files
+        if ($basename ne "meson.build") {
+            # Check for existence of localized image:
+            # Check for existence of localized image:
+            if ($Localize_images) {
+                (my $localized_imgfile = $imgfile) =~ s|/C/|/$Language/|o;
+                $inc_i18n = (-e $localized_imgfile) ? 1 : 0;
+                $imgfile = $localized_imgfile if -e _;  # if $inc_i18n;
+            }
+            print STDERR "$destfile\n" if $Verbose > 2;
+            # Special case symlinks:
+            if ($Mode[0] eq "symlink") {
+                my $dst_to_src_path = $dst_to_src_symlink_path;
+                # If necessary, change relative path too:
+                $dst_to_src_path =~ s|/C/|/$Language/|o
+                    if ($Localize_images && $imgfile =~ m|/$Language/|o);
+                # Use a relative symlink to image file:
+                $imgfile = catfile($dst_to_src_path, $basename);
+            }
+            my $ok = $exec_copy_command->($imgfile, $destfile);
+            if (!$ok) {
+                set_copy_mode();
+                redo;
+            }
+            ++$Count_all  if $Verbose;
+            ++$Count_i18n if $Verbose and $inc_i18n;
         }
-        print STDERR "$destfile\n" if $Verbose > 2;
-        # Special case symlinks:
-        if ($Mode[0] eq "symlink") {
-            my $dst_to_src_path = $dst_to_src_symlink_path;
-            # If necessary, change relative path too:
-            $dst_to_src_path =~ s|/C/|/$Language/|o
-                if ($Localize_images && $imgfile =~ m|/$Language/|o);
-            # Use a relative symlink to image file:
-            $imgfile = catfile($dst_to_src_path, $basename);
-        }
-        my $ok = $exec_copy_command->($imgfile, $destfile);
-        if (!$ok) {
-            set_copy_mode();
-            redo;
-        }
-        ++$Count_all  if $Verbose;
-        ++$Count_i18n if $Verbose and $inc_i18n;
     } # foreach imgfile
 } # foreach srcdir
 
